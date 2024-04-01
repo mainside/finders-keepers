8fa9b2f561152aa1ef126b9e76a51a6f84eb5991
mainside <mainside.host@gmail.com>
2024-04-01 16:53:50 +0900

    home 대신 indext 로 변경

--- src/clj/mainside/finders_keepers/web/routes/pages.clj
+++ src/clj/mainside/finders_keepers/web/routes/pages.clj
@@ -1,29 +1,32 @@
 (ns mainside.finders-keepers.web.routes.pages
   (:require
-    [mainside.finders-keepers.web.middleware.exception :as exception]
-    [mainside.finders-keepers.web.pages.layout :as layout]
-    [integrant.core :as ig]
-    [reitit.ring.middleware.muuntaja :as muuntaja]
-    [reitit.ring.middleware.parameters :as parameters]
-    [ring.middleware.anti-forgery :refer [wrap-anti-forgery]]))
+   [mainside.finders-keepers.web.middleware.exception :as exception]
+   [mainside.finders-keepers.web.pages.layout :as layout]
+   [integrant.core :as ig]
+   [reitit.ring.middleware.muuntaja :as muuntaja]
+   [reitit.ring.middleware.parameters :as parameters]
+   [ring.middleware.anti-forgery :refer [wrap-anti-forgery]]))
 
 (defn wrap-page-defaults []
   (let [error-page (layout/error-page
-                     {:status 403
-                      :title "Invalid anti-forgery token"})]
+                    {:status 403
+                     :title "Invalid anti-forgery token"})]
     #(wrap-anti-forgery % {:error-response error-page})))
 
 (defn home [request]
   (layout/render request "home.html"))
 
+(defn index-for-github-pages [request]
+  (layout/render request "index.html"))
+
 ;; Routes
 (defn page-routes [_opts]
-  [["/" {:get home}]])
+  [["/" {:get index-for-github-pages}]])
 
 (defn route-data [opts]
   (merge
    opts
-   {:middleware 
+   {:middleware
     [;; Default middleware for pages
      (wrap-page-defaults)
      ;; query-params & form-params
